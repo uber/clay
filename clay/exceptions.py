ca545412f4e33fb9834ef8611520494a929f118b
Aditya Sarawgi <aditya@uber.com>
2013-04-08 15:30:21 -0700

    add the ability to indicate actual error in html or json in flask abort

---
+++ clay/exceptions.py
@@ -0,0 +1,26 @@
+from werkzeug.exceptions import default_exceptions, HTTPException
+from flask import make_response, abort as flask_abort, request
+from flask.exceptions import JSONHTTPException
+
+
+def abort(status_code, body=None, headers={}):
+    """
+    Content negiate the error response.
+
+    """
+
+    if 'text/html' in request.headers.get("Accept", ""):
+        error_cls = HTTPException
+    else:
+        error_cls = JSONHTTPException
+
+    class_name = error_cls.__name__
+    bases = [error_cls]
+    attributes = {'code': status_code}
+
+    if status_code in default_exceptions:
+        # Mixin the Werkzeug exception
+        bases.insert(0, default_exceptions[status_code])
+
+    error_cls = type(class_name, tuple(bases), attributes)
+    flask_abort(make_response(error_cls(body), status_code, headers))
