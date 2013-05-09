f197922a94c2b9a860f3f75361c1f91cf799e493
Dom <dnathe4th@gmail.com>
2013-05-09 16:53:34 -0700

    Added mail to clay's import so we can access clay.mail.sendmail after just importing clay

--- clay/__init__.py
+++ clay/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 __version__ = '2.0.2'
 
 from clay.server import app
-from clay import config, stats, logger
+from clay import config, stats, logger, mail
 
-__all__ = ['app', 'config', 'stats', 'logger']
+__all__ = ['app', 'config', 'stats', 'logger', 'mail']
