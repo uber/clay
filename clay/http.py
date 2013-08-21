3038802b21696317e066936c4b4fb3f37d54f785
James Brown <jbrown@uber.com>
2013-08-20 18:06:34 -0700

    convert to soft tabs

--- clay/http.py
+++ clay/http.py
@@ -11,46 +11,46 @@ log = config.get_logger('clay.http')
 
 
 class Request(urllib2.Request):
-	'''
-	This subclass adds "method" to urllib2.Request
-	'''
-	def __init__(self, url, data=None, headers={}, origin_req_host=None,
-				 unverifiable=False, method=None):
-		urllib2.Request.__init__(self, url, data, headers, origin_req_host,
-								 unverifiable)
-		if headers is None:
-			self.headers = {}
-		self.method = method
+    '''
+    This subclass adds "method" to urllib2.Request
+    '''
+    def __init__(self, url, data=None, headers={}, origin_req_host=None,
+                 unverifiable=False, method=None):
+        urllib2.Request.__init__(self, url, data, headers, origin_req_host,
+                                 unverifiable)
+        if headers is None:
+            self.headers = {}
+        self.method = method
 
-	def get_method(self):
-		if self.method is not None:
-			return self.method
-		if self.has_data():
-			return 'POST'
-		else:
-			return 'GET'
+    def get_method(self):
+        if self.method is not None:
+            return self.method
+        if self.has_data():
+            return 'POST'
+        else:
+            return 'GET'
 
 
 def request(method, uri, headers={}, data=None):
-	'''
-	Convenience wrapper around urllib2
-	'''
-	req = Request(uri, headers=headers, data=data, method=method)
-	if not req.get_type() in ('http', 'https'):
-		raise urllib2.URLError('Only http and https protocols are supported')
+    '''
+    Convenience wrapper around urllib2
+    '''
+    req = Request(uri, headers=headers, data=data, method=method)
+    if not req.get_type() in ('http', 'https'):
+        raise urllib2.URLError('Only http and https protocols are supported')
 
-	try:
-		resp = urllib2.urlopen(req)
-		resp = Response(
-			status=resp.getcode(),
-			headers=resp.headers,
-			data=resp.read())
-		log.debug('%i %s %s' % (resp.status, method, uri))
-	except urllib2.HTTPError, e:
-		resp = Response(
-			status=e.getcode(),
-			headers=e.headers,
-			data=e.read())
-		log.warning('%i %s %s' % (resp.status, method, uri))
+    try:
+        resp = urllib2.urlopen(req)
+        resp = Response(
+            status=resp.getcode(),
+            headers=resp.headers,
+            data=resp.read())
+        log.debug('%i %s %s' % (resp.status, method, uri))
+    except urllib2.HTTPError, e:
+        resp = Response(
+            status=e.getcode(),
+            headers=e.headers,
+            data=e.read())
+        log.warning('%i %s %s' % (resp.status, method, uri))
 
-	return resp
+    return resp
