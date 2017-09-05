2563559a10805faeae9decaa6dccf39a691c85f0
Lucas Li <lzl@uber.com>
2017-09-05 14:28:58 -0700

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ util = util  # make pyflakes/etc happy
 
 setup(
     name='clay-flask',
-    version='2.1.6',
+    version='2.1.7',
     author='Jeremy Grosser',
     author_email='jeremy@uber.com',
     packages=['clay'],
